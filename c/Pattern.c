7f1f19533c59f7f694faa45e24c828fa458a2638
SURYACODER <60681084+SURYACODER@users.noreply.github.com>
2021-10-02 20:42:12 +0530

    Create Pattern.c

---
+++ c/Pattern.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i,w,p;
+  char str[]="SURYA";
+  printf("\n");
+  for(i=0;i<5;i++)
+  {
+    p=i+1;
+    printf("\n%-5.*s",p,str);
+  }
+  printf("\n")
+  for(i=4;i>=0;i--)
+  {
+    p=i+1;
+    printf("\n%-5.*s",p,str);
+  }
+  getch();
+  return 0;
+}
