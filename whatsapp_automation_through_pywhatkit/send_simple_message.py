42ba3d8eb1f5ad9b374c88bbfa948cef2adbcb15
Parmanand Patidar <56157826+parmanand99@users.noreply.github.com>
2021-10-03 23:05:16 +0530

    Delete whatsapp_automation_through_pywhatkit directory

--- whatsapp_automation_through_pywhatkit/send_simple_message.py
+++
@@ -1,3 +0,0 @@
-import pywhatkit as kit
-kit.sendwhatmsg_instantly("+91xxxxxxxxxxx", "sending instant message ") # send a message instantly 
-kit.sendwhatmsg("+91xxxxxxxxxx"," sending scheduled message " , 14,59)  #send a message at specified hour and minute in 24 hour format
