5705f5d207294ceaec6926bdaeca502860033b09
Aakas-h <76067278+Aakas-h@users.noreply.github.com>
2021-10-04 23:18:45 +0530

    added new app in flutter

---
+++ flutter/whatasapp_status/android/app/src/main/kotlin/com/example/whatasapp_status/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.whatasapp_status
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/whatasapp_status/lib/Bloc/MainScreenBloc/MainScreenEvent.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:whatasapp_status/Bloc/Bloc.dart';
+
+abstract class MainScreenEvent extends Event{}
+
+class InitilizeFiles extends MainScreenEvent{}
+
+class RefreshFiles extends MainScreenEvent{}
+
