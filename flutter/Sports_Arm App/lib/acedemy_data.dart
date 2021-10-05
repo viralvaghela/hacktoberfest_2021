8fdf1744ef9da91bdc754b4c4f91f94ac23ab5d5
DeepJani-77 <38395492+DEEP-77@users.noreply.github.com>
2021-10-05 20:47:21 +0530

    Add files via upload

---
+++ flutter/Sports_Arm App/lib/acedemy_data.dart
@@ -0,0 +1,11 @@
+class Acedemy {
+  String title;
+  int players;
+  int coaches;
+  String game;
+  String centerHead;
+  String location;
+
+  Acedemy(
+      this.title, this.players, this.coaches,this.game, this.centerHead, this.location);
+}
