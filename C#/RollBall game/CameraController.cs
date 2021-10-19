911591fbaa897270387d11aed6650541723e332c
areenoverclouds <kaurareen@gmail.com>
2021-10-19 12:51:00 +0530

    added c# files

---
+++ C#/RollBall game/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ C#/RollBall game/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    // Before rendering each frame..
+    void Update () 
+    {
+        //rotating prefabs
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+    }
+}   
