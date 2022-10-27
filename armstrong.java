a90b961ee752b399bd923c6e06e14ed420b4ca23
Chandra mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2022-10-27 14:39:09 +0530

    An Armstrong Number
    An Armstrong number is a number whose sum of cubes of its digits is equal to the original number itself .

---
+++ armstrong.java
@@ -0,0 +1,20 @@
+public class armstrong {
+
+    public static void main(String[] args) {
+
+        int num = 370, number, temp, total = 0;
+
+        number = num;
+        while (number != 0)
+        {
+            temp = number % 10;
+            total = total + temp*temp*temp;
+            number /= 10;
+        }
+
+        if(total == num)
+            System.out.println(num + " is an Armstrong number");
+        else
+            System.out.println(num + " is not an Armstrong number");
+    }
+}
